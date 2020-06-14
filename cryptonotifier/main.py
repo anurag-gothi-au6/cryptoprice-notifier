c8de03bd6ca4ef6d2aa60adef452f1c751861189
anurag-gothi-au6 <advisoranurag@gmail.com>
2020-06-14 17:59:28 +0530

    Update

--- cryptonotifier/main.py
+++ cryptonotifier/main.py
@@ -1,14 +1,13 @@
 # Importing All Required Packages
-import requests
 import time
-import json
 import argparse
-import smtplib
-from email.mime.multipart import MIMEMultipart
 from datetime import datetime
-from random import randint
 import os
 import re
+from getdata import crypto_price
+from emergency import emergency_update
+from formatter import response_formatter
+from notifier import notifier, post_ifttt_twitter
 os.system("")
 
 
@@ -24,166 +23,6 @@ class style():
     WHITE = '\033[37m'
     UNDERLINE = '\033[4m'
     RESET = '\033[0m'
-
-
-# Webhooks and Api URL
-BITCOIN_URL = 'https://tinyurl.com/cryptoprice'
-news_url = 'https://tinyurl.com/newsapicrypto'
-
-ifttt_url = 'https://maker.ifttt.com/trigger/'
-ifttt_key = '/with/key/oQJ5UgozawRtt1mcQZqAA9HOGRJc_b4dXHzRanOjAqP'
-twitter_key = '/with/key/bJ6Woc9R6E4B5-Y8PRxQo3a24MZP7_9s1G4qkr0HB26'
-
-IFTTT_WEBHOOKS_IFTTT = ifttt_url + 'ifttt_updates' + ifttt_key
-IFTTT_WEBHOOKS_TELEGRAM = ifttt_url + 'bitcoin_price_update' + ifttt_key
-IFTTT_WEBHOOK_TWITTER = ifttt_url + 'twitter_news' + ifttt_key
-
-
-# setting the http headers
-headers = {
-    'Accepts': 'application/json',
-    # coinmarketcap API key
-    'X-CMC_PRO_API_KEY': '37542254-48e1-41da-8081-1aa8b4e1dab8',
-}
-
-
-# Function to collect Crypto Price In INR
-def crypto_price(coin, curr):
-    curr = curr.upper()
-
-    # parameters to be sent to crypto API
-    parameters = {
-        'start': '1',
-        'limit': '4',
-        'convert': curr
-    }
-
-    session = requests.Session()
-    session.headers.update(headers)
-    response = session.get(BITCOIN_URL, params=parameters)
-    data = json.loads(response.text)
-    if(coin.lower() == 'btc'):
-        price = float(data['data'][0]['quote'][curr]['price'])
-
-    elif(coin.lower() == 'xrp'):
-        price = float(data['data'][3]['quote'][curr]['price'])
-
-    elif(coin.lower() == 'eth'):
-        price = float(data['data'][1]['quote'][curr]['price'])
-
-    else:
-        price = float(data['data'][0]['quote'][curr]['price'])
-
-    return round(price, 2)
-
-
-# function to send email for emergency price alert
-def emergency_update(email, coin, price):
-    recipent_email_address = email
-
-    msg = MIMEMultipart()
-
-    password = 'roxqzeiivwwuvxgv'
-    msg['From'] = 'bitcoinupdate2020@gmail.com'
-    msg['To'] = recipent_email_address
-
-    message = """From:bitcoinupdate2020@gmail.com
-To: {}
-Subject:{} Price Below Threshold
-{} Price crossed below Threshold\n
-Current Price: {} ,\n
-Buy Or Sell Now.\n
-Also Join us here\n
-Telgram: https://t.me/projectcomplete\n
-Twitter: https://twitter.com/BitcoinUpdate2\n\n
-Regards Anurag Gothi
-""".format(email, coin.upper(), coin.upper(), price)
-
-    # Stablishing the gmail sever to send mails
-    server = smtplib.SMTP('smtp.gmail.com: 587')
-
-    server.starttls()
-
-    # Login using gmail account
-    server.login(msg['From'], password)
-
-    # sending the mail
-    server.sendmail(msg['From'], msg['To'], message.encode('utf-8'))
-
-    server.quit()
-
-    print('Emergency Update Sent\n\n')
-
-
-def notifier(event, value, coin):
-    data = {'value1': value, 'value2': coin}
-    # Will be sent to specifed Destination
-    if event == 'ifttt_updates':
-        ifttt_event_url = IFTTT_WEBHOOKS_IFTTT
-
-    else:
-        ifttt_event_url = IFTTT_WEBHOOKS_TELEGRAM
-
-    # HTTP POST request to the webhook URL
-    requests.post(ifttt_event_url, json=data)
-    print('Response sent to Destination\n\n')
-
-
-# Function to format the Crypto Price Response for different destinations
-def response_formatter(crypto_logs, event, curr):
-    rows = []
-    curr = curr.upper()
-
-    # Currency Symbol Formatter
-    symbol = '₹'
-    if curr == 'INR':
-        symbol = '₹'
-    elif curr == 'EUR':
-        symbol = '€'
-    elif curr == 'USD':
-        symbol = '$'
-    elif curr == 'GBP':
-        symbol = '£'
-
-    if(event == 'telegram'):
-        for crypto_value in crypto_logs:
-            date = crypto_value['date'].strftime('%d.%m.%Y %H:%M')
-            value = crypto_value['crypto_current_price']
-            row = '{}: {} <b>{}</b>'.format(date, symbol, value)
-            rows.append(row)
-        data = '<br>'.join(rows)
-        data += '<br><br>Happy Earning'
-        return data
-    else:
-        for crypto_value in crypto_logs:
-            date = crypto_value['date'].strftime('%d.%m.%Y %H:%M')
-            value = crypto_value['crypto_current_price']
-            row = '{}: {} {}'.format(date, symbol, value)
-            rows.append(row)
-        data = '\n'.join(rows)
-        data += '\n\nHappy Earning'
-        return data
-
-
-# Function to collect News Using News API
-def news_alert():
-    session = requests.Session()
-    response = session.get(news_url)
-    newss = json.loads(response.text)
-    index = randint(0, 19)
-    news = newss['articles'][index]
-    data = 'Quick News: {}\nBy: {}\n{}'.format(
-        news['title'], news['author'], news['url'])
-
-    return data
-
-
-# Function that Posts News To Twitter
-def post_ifttt_twitter():
-    data = {'value1': news_alert()}
-    post_event = IFTTT_WEBHOOK_TWITTER
-    requests.post(post_event, json=data)
-    print('News Tweet been sent\n\n')
 
 
 # Function which collects price and send a notification to dest
@@ -349,5 +188,5 @@ setting it to btc\n''')
 
 
 # Commenting main function As it is being called with the Python Package
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
